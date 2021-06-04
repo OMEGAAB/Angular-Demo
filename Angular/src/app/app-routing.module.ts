1d12a4c36d7c2a184bbdf96d4b07fdd785ff7fa6
AB <abhishekbaboon@gmail.com>
2021-06-04 10:49:09 +0530

    C2

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { PartnerlistComponent } from './partnerlist/partnerlist.component';
-import { StudentListComponent } from './student-list/student-list.component';
+
 
 
 
@@ -10,7 +8,6 @@ import { AddStudentComponent } from './add-student/add-student.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'view-student', pathMatch: 'full' },
-  { path: 'view-student', component: StudentListComponent },
   { path: 'add-student', component: AddStudentComponent },
 ];
 
--- Eclipse/Student/Student/src/main/java/Service/Student_Service_Imp.java
+++ Eclipse/Student/Student/src/main/java/Service/Student_Service_Imp.java
@@ -1,11 +1,8 @@
 package Service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import DAO.Student_DAO;
 import Model.Student;
 
@@ -18,27 +15,6 @@ public class Student_Service_Imp implements Student_Service {
 	
 	@Override
 	public boolean saveStudent(Student student) {
-		return studentdao.saveStudent(student);   /* this line is responsible for inserting value to the databse*/
+		return studentdao.saveStudent(student);
 	}
-
-	@Override
-	public List<Student> getStudents() {
-		return studentdao.getStudents();
-	}
-
-	@Override
-	public boolean deleteStudent(Student student) {
-		return studentdao.deleteStudent(student);
-	}
-
-	@Override
-	public List<Student> getStudentByID(Student student) {
-		return studentdao.getStudentByID(student);
-	}
-
-	@Override
-	public boolean updateStudent(Student student) {
-		return studentdao.updateStudent(student);
-	}
-
 }
